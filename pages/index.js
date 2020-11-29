d45b731e9bbb6929a458ae5316f7eaaec9a6c3fa
越努力越幸运✍ <snbckcode@gmail.com>
2020-11-29 08:37:38 +0000

    feat: head

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import Head from 'next/head'
+// Home
+
+import {head} from "../components/head"
 
 export default function Home() {
   return (
     <div className="container">
 
-      <Head>
-        <title>Welcome to UA Framework</title>
-        <link rel="icon" href="https://avatars3.githubusercontent.com/u/73245034?s=90&v=4" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css" />
-      </Head>
+      <componentsHead title="Welcome to UA Framework"/>
 
       <main>
         <h1 align="center" className="title">
@@ -34,8 +30,6 @@ export default function Home() {
           🎃 Powered by IsClub
         </a>
       </footer>
-
-
 
 
     </div>
